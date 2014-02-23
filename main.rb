31a85e8874728eb3a0515cd3d59b8cf97f221155
Sunil Khutan <sdk4@hw.ac.uk>
2014-02-23 14:14:28 +0000

    added comma separation for number output

--- main.rb
+++ main.rb
@@ -3,8 +3,10 @@ require 'fx'
 require 'slim'
 require 'bigdecimal'
 
-@@currencies = FX::ExchangeRate.currencies
-@@dates = FX::ExchangeRate.dates
+before do
+  @@currencies ||= FX::ExchangeRate.currencies
+  @@dates ||= FX::ExchangeRate.dates
+end
 
 get '/' do
   slim :index
@@ -14,11 +16,17 @@ post '/' do
   @date = params[:date]
   @from = params[:from]
   @to = params[:to]
-  @amount = BigDecimal.new(params[:amount])
-  @result = (FX::ExchangeRate.at(@date, @from, @to) * @amount).round(2).to_s('F')
-  @result = @amount if @from == @to
-  @amount = @amount.to_s('F')
+  @amount = params[:amount]
+  @result = (FX::ExchangeRate.at(@date, @from, @to) * BigDecimal.new(@amount)).round(2).to_s('F')
+  @amount = comma_sep(@amount)
+  @result = (@amount if @from == @to) || comma_sep(@result)
   slim :index
 end
 
 get('/styles.css'){ content_type 'text/css', :charset => 'utf-8' ; scss :styles }
+
+def comma_sep(s)
+  whole, decimal = s.split(".")
+  whole_with_commas = whole.chars.to_a.reverse.each_slice(3).map(&:join).join(",").reverse
+  [whole_with_commas, decimal].compact.join(".")
+end
